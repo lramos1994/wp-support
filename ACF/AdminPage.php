0c9dd5c39a95f47060cef18069f74a031ead650b
Lucas ramos de oliveira <lucas_ramos1994@hotmail.com>
2017-11-08 17:15:49 -0200

    Initial Commit

---
+++ ACF/AdminPage.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace StudioVisual\Support\ACF;
+
+class AdminPage
+{
+    protected $parent;
+
+    public function add($title, $is_parent = true)
+    {
+        $page_title = $title;
+        $menu_title = $title;
+
+        if (is_array($title)) {
+            $menu_title = $title[1];
+        }
+
+        if (!function_exists('acf_add_options_page')) {
+            return new WP_Error('dependency', MISSING_ACF);
+        }
+
+        if ($is_parent) {
+            return $this->parent = acf_add_options_page([
+                'page_title' => $page_title,
+                'menu_title' => $menu_title,
+                'redirect' => true,
+            ]);
+        }
+
+        if (!$is_parent) {
+            return acf_add_options_sub_page([
+                'page_title' => $page_title,
+                'menu_title' => $menu_title,
+                'parent_slug' => $this->parent['menu_slug'],
+            ]);
+        }
+    }
+
+    public function setParentSlug($parent_slug)
+    {
+        $this->parent['menu_slug'] = $parent_slug;
+    }
+
+    public function getParentSlug()
+    {
+        return $this->parent['menu_slug'];
+    }
+}
